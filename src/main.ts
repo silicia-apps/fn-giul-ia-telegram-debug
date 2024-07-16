73239f13b6ff45d99a5559c0b3a79829c698d095
Francesco Bozzotta (aka xFr4xx) <francesco.bozzotta@gmail.com>
2024-07-16 21:17:42 +0200

    add store user info

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { Client } from 'node-appwrite';
+import { Client, Databases, Query, ID } from 'node-appwrite';
 import { Telegraf } from 'telegraf';
-import { message } from 'telegraf/filters'
+import { message } from 'telegraf/filters';
 
 //import * as process from './env.js';
 
@@ -19,32 +19,70 @@ type Context = {
 };
 
 export default async ({ req, res, log, error }: Context) => {
+  try {
+    log('connect to Telegram Bot');
+    const bot = new Telegraf(process.env.TELEGRAM_TOKEN!);
+    log('connect to appwrite api');
+    const client = new Client()
+      .setEndpoint('https://cloud.appwrite.io/v1')
+      .setProject(process.env.APPWRITE_PROJECT_ID!)
+      .setKey(process.env.APPWRITE_API_KEY!);
+    let datastore = new Databases(client);
+    let chat = await datastore.listDocuments(
+      process.env.APPWRITE_DATABASE_ID!,
+      process.env.APPWRITE_TABLE_CHATS_ID!,
+      [
+        Query.equal('channel', 'telegram'),
+        Query.equal('chat_id', req.body.message.chat.id),
+        Query.limit(1),
+      ]
+    );
+    switch (req.body.message.text) {
+      case '/start':
+        log('Registrazione Bot');
+        bot.telegram.sendMessage(req.body.message.chat.id, 'Benvenuto');      
+        if (chat.total === 0) {
+          log('User not present');
+          const new_user = {
+            memory: {},
+            name: req.body.message.from.username,
+            chats: { channel: 'telegram' , 'chat_id': req.body.message.chat.id}
+          }
+          log (`write new user ${new_user}`);
+          await datastore.createDocument(
+            process.env.APPWRITE_DATABASE_ID!,
+            process.env.APPWRITE_TABLE_IDENTITIES_ID,
+            ID.unique(),
+            new_user,
+          );
+          log (`user created`);
+        } else {
+          log (`user already in database`);
+        }
+        break;
+      default:
+        bot.telegram.sendMessage(
+          req.body.message.chat.id,
+          req.body.message.text
+        );
+        console.log(req.body);
+    }
 
-  const bot = new Telegraf(process.env.TELEGRAM_TOKEN!);
+    // The `req` object contains the request data
+    if (req.method === 'GET') {
+      // Send a response with the res object helpers
+      // `res.send()` dispatches a string back to the client
+      return res.send('Hello, World!');
+    }
 
-  switch (req.body.message.text) {
-    case '/start': log('Start Telegram Bot');
-
-      bot.telegram.sendMessage(req.body.message.chat.id, 'Benvenuto');
-    break;
-    default:
-      bot.telegram.sendMessage(req.body.message.chat.id, req.body.message.text);
-      console.log(req.body);
+    // `res.json()` is a handy helper for sending JSON
+    return res.json({
+      motto: 'Build like a team of hundreds_',
+      learn: 'https://appwrite.io/docs',
+      connect: 'https://appwrite.io/discord',
+      getInspired: 'https://builtwith.appwrite.io',
+    });
+  } catch (e: any) {
+    error(e);
   }
-
-  
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
 };
