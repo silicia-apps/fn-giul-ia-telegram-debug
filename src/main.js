934cf7b71b4fecb3cd6a2f328c63e5c8faa9f162
Francesco Bozzotta <39646929+silicia-apps@users.noreply.github.com>
2024-07-16 13:23:59 +0200

    3

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import { Client } from 'node-appwrite';
 
 export default async ({ req, res, log, error }) => {
 
-  switch (req.body.entities.type) {
+  /*switch (req.body.entities.type) {
     'bot_command': log('Inviato comando al bot');
     break;
     default:
       log('messaggio');
-  }
+  }*/
 
   
   // The `req` object contains the request data
