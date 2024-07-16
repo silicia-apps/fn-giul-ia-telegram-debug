cb6a75b2b68b0f8539581830980a5b68d2322d0a
Francesco Bozzotta <39646929+silicia-apps@users.noreply.github.com>
2024-07-16 13:17:04 +0200

    1

--- src/main.js
+++ src/main.js
@@ -1,21 +1,10 @@
 import { Client } from 'node-appwrite';
 
-// This is your Appwrite function
-// It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+  log(req);
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
-
+  
   // The `req` object contains the request data
   if (req.method === 'GET') {
     // Send a response with the res object helpers
